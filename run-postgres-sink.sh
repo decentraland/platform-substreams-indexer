42bf5649a373ddc0b036c6d3e30141e55cbb8ebd
Pedro Tamborindeguy <pftambo@gmail.com>
2023-06-21 15:44:15 -0300

    connect to dev instance

--- run-postgres-sink.sh
+++ run-postgres-sink.sh
@@ -1,5 +1,10 @@
+PORT="5432"
+if [ $1 = "dev" ]; then
+   PORT="5431"
+fi
+
 ./substreams-sink-postgres run \
-    "psql://substreams_admin:pass@localhost:5432/substreams_admin?sslmode=disable" \
+    "psql://substreams_admin:pass@localhost:$PORT/substreams_admin?sslmode=disable" \
     "polygon.streamingfast.io:443" \
     "substreams.yaml" \
     --development-mode=true \
