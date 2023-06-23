371fe82e2d8f7784b728a22e045ab0523460859d
Pedro Tamborindeguy <pftambo@gmail.com>
2023-06-23 17:13:50 -0300

    fix script

--- connect-docker-db.sh
+++ connect-docker-db.sh
@@ -1,1 +1,1 @@
-docker exec -e PGPASSWORD=pass -it substreams  psql -h 127.0.0.1 -p 5432 -U substreams_admi
+docker exec -e PGPASSWORD=pass -it substreams  psql -h 127.0.0.1 -p 5432 -U substreams_admin
